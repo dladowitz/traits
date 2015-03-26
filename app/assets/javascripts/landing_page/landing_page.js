54226ea86f70c6b5228a9979733fd927b858549a
David Ladowitz <david@ladowitz.com>
2015-03-25 20:05:38 -0700

    fixing fontaweome issues and more css business

---
+++ app/assets/javascripts/landing_page/landing_page.js
@@ -0,0 +1,13 @@
+// This is a manifest file that'll be compiled into landing_page.js, which will include all the files
+// listed below.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+// There are a number of JS files in this folder from the start bootstrap template.
+// Probably not being used right now but needed for full template functionaily
+
+//= require_tree .
